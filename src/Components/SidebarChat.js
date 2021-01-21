173db8e41327950cc87a855d70b36328328b9b56
Bishesh <bishesh.sunam@gmail.com>
2021-01-22 00:15:54 +1300

    fixed last seen message on chat room

--- src/Components/SidebarChat.js
+++ src/Components/SidebarChat.js
@@ -7,6 +7,22 @@ import { Link } from 'react-router-dom';
 function SidebarChat({ addNewChat, id, name }) {
 
     const [seed, setSeed] = useState(" ");
+    const [messages, setMessages] = useState("")
+
+    //pull message from db to show last message on screen
+    useEffect(() => {
+        if(id){
+            db.collection("rooms")
+            .doc(id)
+            .collection("messages")
+            .orderBy('timestamp','desc')
+            .onSnapshot((snapshot)=>
+                setMessages(snapshot.docs.map((doc)=>
+                doc.data()
+                ))
+            )
+        }
+    }, [])
 
     useEffect(() => {
        setSeed(Math.floor(Math.random() * 5000));
@@ -31,7 +47,7 @@ function SidebarChat({ addNewChat, id, name }) {
             />
             <div className="sidebarChat__info">
                 <h2>{name}</h2>
-                <p>Last message...</p>
+                <p>{messages[0]?.message}</p>
             </div>
         </div>
      </Link>
